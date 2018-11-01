5284de0e2ed2e24fb7d9f15b3425337c161d2c84
Patrick <pats.carlson@gmail.com>
2018-10-31 22:08:49 -0400

    changes

--- react/src/MainNavbar.js
+++ react/src/MainNavbar.js
@@ -1,10 +1,12 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
+import img from "../static/images/SleepingPanda.gif";
 
-const navbarStyle = {
-    backgroundColor: "#ce1616",
-    // margin: "0px",
-}
+
+// const navbarStyle = {
+//     backgroundColor: "#ce1616",
+//     // margin: "0px",
+// }
 
 
 
@@ -12,11 +14,21 @@ const navbarStyle = {
 class MainNavbar extends Component{
     render(){
         return (
-            <nav className="navbar" style={navbarStyle} role="navigation" arai-label="main-navigation">
+            <nav className="navbar is-black" role="navigation" arai-label="main-navigation">
                 <div className="container">
                     <div className="navbar-brand">
                         <a className="navbar-item">
-                            <i className="fab fa-rebel icon is-large"></i>
+                            {/* <i className="fab fa-rebel icon is-large"></i> */}
+                            <img src={img} alt="Juniped" width="30" height="30" />
+
+                        </a>
+                    </div>
+                    <div className="navbar-menu">
+                        <a href="/" className="navbar-item has-text-warning">
+                            Home
+                        </a>
+                        <a href="/nothome" className="navbar-item has-text-warning">
+                            Not Home
                         </a>
                     </div>
                 </div>
